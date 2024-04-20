8b4b866fd7f877a0d82e76360ab2ee3ef4bb2bf9
Alexandre Delporte Alexandre.Delporte@univ-grenoble-alpes.fr <delporta@f-dahu.u-ga.fr>
2024-04-20 15:16:54 +0200

    push results of the simulation study launched on the cluster dahu

---
+++ R/simulation_study/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# -- Un nom pour le job --
+#OAR -n simulation_study_rect
+# -- Description des ressources souhaitées -
+#OAR -l /core=96,walltime=05:00:00
+# -- Et bien sur, le projet Perseus --
+#OAR --project pr-formation-ced-calcul
+
+# -- Puis la liste des commandes à exécuter --
+source /applis/site/guix-start.sh
+R -e "rmarkdown::render('simulation_study_rect.Rmd')"
