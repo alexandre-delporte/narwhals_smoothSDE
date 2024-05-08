e3e752d89ac167de03fde5eb51d6654ef97563bc
Alexandre Delporte <alexdelporte83@gmail.com>
2024-05-08 07:05:51 +0200

    preproseccing results with modified land polygons

--- R/preprocessing/process_boundaries.R
+++ R/preprocessing/process_boundaries.R
@@ -3,6 +3,7 @@
 #' We obtain a spatial features object (R package sf) that is more convenient to manipulate.
 
 
+#Check Geological Survey of Denmark and Greenland (GEUS)
 
 #to get polygons for the shore
 #library(jsonlite)
@@ -24,14 +25,13 @@ coastline<-st_read(file.path(data_path,"coastline.geojson"))
 land<-st_read(file.path(data_path,"land.shp"))
 
 
-#other data from https://ramadda.data.bas.ac.uk/repository/entry/show?entryid=8cecde06-8474-4b58-a9cb-b820fa4c9429
-land_gerrish=st_read(file.path(data_path,"Greenland_coast","Greenland_coast.shp"))
-
+land_google=st_read(file.path(data_path,"scoresby_sound.shp"))
+st_crs(land_google)="+init=EPSG:32626"
 
 #convert long lat coordinates to UTM zone 26
 coastline_utm <- st_transform(coastline, crs = "+init=EPSG:32626 +units=km")
 land_utm <- st_transform(land, crs = "+init=EPSG:32626 +units=km")
-land_gerrish_utm=st_transform(land_gerrish)
+land_google_utm <- st_transform(land_google, crs = "+init=EPSG:32626 +units=km")
 
 
 #crop again to actually get a rectangle in UTM coordinates
@@ -48,7 +48,9 @@ bbox_polygon <- st_as_sfc(st_bbox(bbox)
 
 land_utm<- land_utm %>% st_intersection(bbox_polygon)
 coastline_utm<- coastline_utm %>% st_intersection(bbox_polygon)
+land_google_utm<- land_google_utm %>% st_intersection(bbox_polygon)
 
 #save the sf object in the Data directory
 st_write(coastline_utm,file.path(data_path,"coastline_utm.geojson"),append=FALSE)
 st_write(land_utm,file.path(data_path,"land_utm.shp"),append=FALSE)
+st_write(land_google_utm,file.path(data_path,"land_google_utm.shp"),append=FALSE)
